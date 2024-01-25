9acbfa5d7a0a53bca1b2892be28e32a6fc5df9cb
Alperen <alperensarisan@icloud.com>
2024-01-25 16:09:37 -0400

    Basic stuff in the detail screen is done, now more complex stuff

---
+++ JP Apex Predators/PredatorDetail.swift
@@ -0,0 +1,18 @@
+//
+//  PredatorDetail.swift
+//  JP Apex Predators
+//
+//  Created by Alperen Sarışan on 25.01.2024.
+//
+
+import SwiftUI
+
+struct PredatorDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PredatorDetail()
+}
